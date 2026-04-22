c2212db509995d5fd9660b5e0742ae8c24d0f5a4
Caio Cordeiro <caiocordeirodossantoss@gmail.com>
2026-04-22 11:53:41 -0300

    Delete Exercicio1.java

--- Exercicio1.java
+++
@@ -1,8 +0,0 @@
-public class Exercicio1 {
-    public static double teoremaPitagoras (double a, double b) {
-    return Math.sqrt((a*a) + (b*b));
-    }
-    public static void main(String[] args) {
-        System.out.println(teoremaPitagoras(4, 3));
-    }
-}
